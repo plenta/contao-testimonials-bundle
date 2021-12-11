3529c6ab27712cb57afe40c3e97e5b193479c716
christianbarkowsky <hallo@brkwsky.de>
2021-12-11 14:58:27 +0100

    #1

--- src/Plenta/ContaoTestimonialsBundle/EventListener/DataContainerListener.php
+++ src/Plenta/ContaoTestimonialsBundle/EventListener/DataContainerListener.php
@@ -12,56 +12,32 @@ declare(strict_types=1);
 
 namespace Plenta\ContaoTestimonialsBundle\EventListener;
 
-use Codefog\TagsBundle\Manager\ManagerInterface;
-use Codefog\TagsBundle\Tag;
-use Contao\Backend;
-use Contao\BackendUser;
-use Contao\Controller;
-use Contao\CoreBundle\Exception\AccessDeniedException;
-use Contao\CoreBundle\Exception\ResponseException;
-use Contao\CoreBundle\Monolog\ContaoContext;
-use Contao\DataContainer;
-use Contao\Environment;
-use Contao\Image;
-use Contao\Input;
-use Contao\StringUtil;
-use Contao\System;
-use Contao\Validator;
 use Doctrine\DBAL\Connection;
-use Haste\Model\Model;
-use Psr\Log\LoggerInterface;
-use Psr\Log\LogLevel;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBagInterface;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Terminal42\NodeBundle\Model\NodeModel;
-use Terminal42\NodeBundle\PermissionChecker;
-use Terminal42\NodeBundle\Widget\NodePickerWidget;
 
 class DataContainerListener
 {
-    private Connection $db;
+    private Connection $connection;
 
-    public function __construct(Connection $db)
+    public function __construct(Connection $connection)
     {
-        $this->db = $db;
+        $this->connection = $connection;
     }
 
     /**
+     * @return array
      * @throws \Doctrine\DBAL\Exception
      */
     public function onTestimonialsOptionsCallback(): array
     {
-        $arr = [];
-        $nodes = $this->db->fetchAllAssociative("SELECT t.id AS id, t.name AS name, a.title AS parent FROM tl_testimonials AS t LEFT JOIN tl_testimonials_archive AS a ON t.pid = a.id WHERE t.published=1 ORDER BY t.name ASC");
+        $testimonials = [];
+        $results = $this->connection->fetchAllAssociative("SELECT t.id AS id, t.name AS name, a.title AS parent FROM tl_testimonials AS t LEFT JOIN tl_testimonials_archive AS a ON t.pid = a.id WHERE t.published=1 ORDER BY t.name ASC");
 
-        if (is_array($nodes)) {
-            foreach ($nodes as $node) {
-                $arr[$node['parent']][$node['id']] = $node['name'];
+        if (is_array($results)) {
+            foreach ($results as $result) {
+                $testimonials[$result['parent']][$result['id']] = $result['name'];
             }
         }
 
-        return $arr;
+        return $testimonials;
     }
 }
