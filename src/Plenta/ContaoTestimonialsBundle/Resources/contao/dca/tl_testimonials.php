64d2ee704f6f1d411d76161211dfd36be67d97c5
christianbarkowsky <hallo@brkwsky.de>
2022-12-08 13:58:42 +0100

    #11

--- src/Plenta/ContaoTestimonialsBundle/Resources/contao/dca/tl_testimonials.php
+++ src/Plenta/ContaoTestimonialsBundle/Resources/contao/dca/tl_testimonials.php
@@ -56,7 +56,7 @@ $GLOBALS['TL_DCA']['tl_testimonials'] = [
             'delete' => [
                 'href' => 'act=delete',
                 'icon' => 'delete.svg',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\'))return false;Backend.getScrollOffset()"',
             ],
             'toggle' => [
                 'icon'                => 'invisible.svg',
