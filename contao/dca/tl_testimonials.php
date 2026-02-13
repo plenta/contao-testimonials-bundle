8266893f5eb52995682ba35348f85e738fa7a73e
Christian Barkowsky <christian@plenta.io>
2026-02-13 10:48:42 +0100

    dca class added

--- contao/dca/tl_testimonials.php
+++ contao/dca/tl_testimonials.php
@@ -190,7 +190,7 @@ $GLOBALS['TL_DCA']['tl_testimonials'] = [
             'inputType' => 'checkbox',
             'eval' => [
                 'doNotCopy' => true,
-                'tl_class' => 'w50',
+                'tl_class' => 'w50 m12',
             ],
             'sql' => "char(1) NOT NULL default ''",
         ],
